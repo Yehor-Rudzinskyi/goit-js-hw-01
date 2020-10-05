05d89289dd84943e0c153aa73b58c541612b26fe
Yehor <65769717+Yehor-Rudzinskyi@users.noreply.github.com>
2020-10-05 22:42:41 +0300

    add

--- js/task_3_1.js
+++ js/task_3_1.js
@@ -35,12 +35,20 @@
 const findBestEmployee = function (employees) {
   const values = Object.values(employees);
   let max = Math.max(...values);
+  let message = '';
+  const names = Object.keys(employees);
 
-  for (const key in employees) {
-    if (employees[key] === max) {
-      return key;
+  for (const name of names) {
+    if (employees[name] === max) {
+      return (message = name);
     }
   }
+  // for (const key of employees) {
+  //   if (employees[key] === max) {
+  //     return (message = key);
+  //   }
+  // }
+  return message;
 };
 
 const developers = {
@@ -50,3 +58,103 @@ const developers = {
   lorence: 99,
 };
 console.log(findBestEmployee(developers));
+
+//  TASK 4
+
+// const countTotalSalary = function (employees) {
+//   let arrowOfNumbers = Object.values(employees);
+//   console.log(arrowOfNumbers);
+//   let totalSalary = 0;
+//   for (const number of arrowOfNumbers) {
+//     totalSalary += number;
+//   }
+//   return totalSalary;
+// };
+
+// // Объекты и ожидаемый результат
+// const developers = {
+//   mango: 300,
+//   poly: 250,
+//   alfred: 450,
+// };
+// console.log(countTotalSalary(developers));
+// // 1000
+
+// const supports = {
+//   kiwi: 200,
+//   lux: 150,
+//   chelsy: 150,
+// };
+// console.log(countTotalSalary(supports));
+
+//  TASK 5
+// к нам приходят объект и его ключи
+// мы должны вернуть массив значений свойства которое нам прилетает из аргумента
+// 1. Перебиаем массив объектов
+// 2. С помощью in проверяем, есть ли значение у свойства
+// 3. Условием, если true - пушим в новый массив
+// function getAllPropValues(array, prop) {
+//   let newArray = [];
+//   for (const key of array) {
+//     if (prop in key) {
+//       newArray.push(key[prop]);
+//     }
+//   }
+//   return newArray;
+// }
+
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Радар', price: 1280, quantity: 2 },
+//   { name: 'Радар', price: 1320, quantity: 1 },
+//   { name: 'Сканер', price: 2700, quantity: 1 },
+//   { name: 'Сканер', price: 2500, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 2 },
+// ];
+
+// console.log(getAllPropValues(products, 'name'));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+
+//console.log(getAllPropValues(products, 'quantity'));
+// [4, 2, 1, 1, 3, 7, 2]
+
+//console.log(getAllPropValues(products, 'category'));
+//  []
+
+// TASK 6
+// К нам прилетает массив объектов и значение свойств, которые нам нужно совокупить
+// Задача, веернуть объщую стоимость сплюсовав свойство price и умеожив на quantity
+// 1. Перебираем массив объектов
+// 2. Сравниваем имя свойства прилитевшее из аргумента с свойствами в массиве
+// 3. Если true - делаем вычисления и запихиваем в ранее созданную переменную
+function calculateTotalPrice(array, prop) {
+  let total = 0;
+  for (const arr of array) {
+    if (arr.name === prop) {
+      total += arr.price * arr.quantity;
+    }
+  }
+  return total;
+}
+
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Радар', price: 1280, quantity: 2 },
+//   { name: 'Радар', price: 1320, quantity: 1 },
+//   { name: 'Сканер', price: 2700, quantity: 1 },
+//   { name: 'Сканер', price: 2500, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 2 },
+// ];
+// console.log(calculateTotalPrice(products, 'Радар'));
+// 9080
+
+//console.log(calculateTotalPrice(products, 'Сканер'));
+// 10200
+
+//console.log(calculateTotalPrice(products, 'Захват'));
+// 2400
+
+//console.log(calculateTotalPrice(products, 'Дроид'));
+// 2800
