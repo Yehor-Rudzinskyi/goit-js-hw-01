aca7e0e384c873268957be06fc68da23ae448a33
Yehor <65769717+Yehor-Rudzinskyi@users.noreply.github.com>
2020-10-22 22:45:09 +0300

    add some tasks

--- js/task_4.1.js
+++ js/task_4.1.js
@@ -31,13 +31,9 @@
 //   for(let i = 0; i < array.length; i += 1) {
 //     const element = array[i];
 //     const index = i;
-//   const pass = cb(element, index, arr)
-//     console.log(pass)
-//     // Write code under this line
-//     if (pass){
-//       numbers.push(element)
-      
-//     }
+//  if (cb(element, index, array)){
+    //   numbers.push(element)
+    // }
 //   }
 //   return numbers;
 // }
@@ -61,32 +57,32 @@
 // в зависимости от того какая именно из трех функция(add, mult, sub) будет передана в качестве cb.
 
 
-const add = (accum, element) => accum + element;
-const mult = (accum, element) => accum * element;
-const sub = (accum, element) => accum - element;
+// const add = (accum, element) => accum + element;
+// const mult = (accum, element) => accum * element;
+// const sub = (accum, element) => accum - element;
 
-function reduceArray(array, cb, initial) {
-  'use strict';
-  let i;
-    let accum;
-    console.log(arguments)
-  if(arguments.length >= 3) {
-    accum = initial;
-    i = 0;
-  }
-  if(arguments.length === 2) {
-    accum = array[0];
-    i = 1;
-  }
-  for(i; i < array.length; i += 1) {
-    const element = array[i];
-    // Write code under this line
-accum = cb(accum, element)
-  }
-  return accum;
-}
+// function reduceArray(array, cb, initial) {
+//   'use strict';
+//   let i;
+//     let accum;
+//     console.log(arguments)
+//   if(arguments.length >= 3) {
+//     accum = initial;
+//     i = 0;
+//   }
+//   if(arguments.length === 2) {
+//     accum = array[0];
+//     i = 1;
+//   }
+//   for(i; i < array.length; i += 1) {
+//     const element = array[i];
+//     // Write code under this line
+// accum = cb(accum, element)
+//   }
+//   return accum;
+// }
 
-const arr  = [1,2,3,4,5];
+// const arr  = [1,2,3,4,5];
 
 // console.log(reduceArray(arr, add)); // 15
 // console.log(reduceArray(arr, add, 10)); // 25
@@ -259,4 +255,170 @@ account.addOrder(5000, 'order-4');
 // console.log(account.getTransactionDetails(100));
 // account.deposit(400);
 // console.log(account.getTransactionTotal(Transaction.DEPOSIT))
-// console.log(account.getTransactionTotal(Transaction.WITHDRAW))
+// console.log(account.getTransactionTotal(Transac.WITHDR
+
+
+// const printValue = function (value) {
+//     console.log(`Value:`, value)
+// }
+
+// CALL BACK FUNCTION
+
+// const newNumber = function (n, worker) {
+//     for (let i = 0; i < n; i += 1) {
+//         worker(i)
+//     }
+// }
+// newNumber(3, value => { console.log(`Hi`, value) })
+// Вызываем функцию, которой передаём значение и работягу.
+// Значение учавствует в переборе.
+// Работяга выводит на консоль передаваемые ему значения.
+
+// ФИЛЬТР МАССИВ!
+
+// const filter = function (array, test) {
+//     const filteredElements = [];
+//     for (const element of array) {
+//         const passed = test(element)
+//         if (passed) {
+//             filteredElements.push(element)
+//         }
+//     }
+//     return filteredElements
+// }
+// const fruits = [
+//   { name: 'apples', quantity: 200, isFresh: true },
+//   { name: 'grapes', quantity: 150, isFresh: false },
+//   { name: 'bananas', quantity: 100, isFresh: true },
+// ];
+// const freshFruits = filter(fruits, fruit => fruit.isFresh);
+// console.log(freshFruits); // массив с объектами apples и bananas
+
+// const fruitsWithQuantity = filter(fruits, fruit => fruit.quantity >= 120);
+// console.log(fruitsWithQuantity); // массив с объектами apples и grapes
+// Для фильтрации массива используем получаемые данные и функцию - проверку,
+//     которая наполнит новый отфильтрованный массив, через условие.
+// Создаём балванку для отфильтровааонного массива.
+// Делаем перебор получаемого массива.
+// Присваиваем переменной результат сверки данных: каждого перебраного элемента поступающего массива,
+//     который мы даём функции работяге, как инструмент.
+// сама же функция - работяга получает объект и заглядывает в его свойства,
+//     чтобы в итоге сравнить значения и отдать переменной результат true or false
+// которую мы используем в условии и наролняем нашу балванку, если условие = true
+// Фильтр Массива возвращает нам в итоге наполненную йбалванку 
+
+
+// ЗАМЫКАНИЕ
+
+// const createCounter = function() {
+
+//   let privateValue = 0;
+
+//   const increment = function() {
+//     privateValue += 1;
+//     console.log(privateValue);
+//   };
+
+//   return {
+//     increment,
+//   };
+// };
+
+// const counterA = createCounter();
+// console.dir(counterA)
+// counterA.increment(); // 1
+// counterA.increment(); // 2
+
+// const counterB = createCounter();
+// counterB.increment(); // 1
+// counterB.increment(); // 2
+// counterB.increment(); // 3
+
+
+// function person() {
+//   let name = 'Peter';
+  
+//   return function displayName() {
+//     console.log(name);
+//   };
+// }
+// let peter = person();
+// peter()
+// В переменную peter мы прсваиваем результат возврата функции person. 
+// Результатаом явдяется возврат функции displayName
+// Имея теперь функцию displayName в переменной peter - ьы вызываем её результат.peter()
+// Функция person уже давно завершила свою работу, а peter до сих пор получает значение её локальной переменной name... 
+// Соответственно функция displayName является ЗАМЫКАНИЕМ
+
+// function getCounter() {
+//     let counter = 0;
+//     return function () {
+//         return counter += 1
+//     }
+// }
+
+// const count = getCounter();
+// console.log(count())
+// console.log(count())
+// console.log(count())
+// console.log(count())
+
+// Имеем функцию с объявленной переменной и анонимной функцией, которая в свою очередь, выполняет арифметическое действие.
+//     Присваиваем(идентифицируем) анонимной функции имя count и выводим в консоль результат её возврата.
+// При каждом вызове функции count, перезаписывается значение counter. 
+// Так происходит, потому что при каждом вызове count(), создаётся новая область видимости,
+//     но есть только одна область видимости, созданная для getCounter,
+//     так как переменная counter объявлена в области видимости getCounter(),
+//     она увеличится при каждом вызове функции count, вместо того, чтобы сброситься до 0.
+
+
+// ПроПУСК Элемента при переборе Массива.
+
+// var arr = [1,2,42,3];
+// var brr = [];
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === 42) {
+//         i++
+// // меняем значение i на +1, пропуская при этом пуш на этот элемент
+        
+//   }
+
+//   brr.push(arr[i]);
+// }
+
+// console.log(brr)
+
+// let purse = 50;
+// const tolerance =  18;
+// const input = 25;
+// const priceChildren = 25;
+// const price = 50;
+// const promo = 0.8;
+// purse -= tolerance  >= input ? priceChildren : price * promo;
+    
+// console.log(purse);
+
+const createCounter = function() {
+  let privateValue = 0;
+
+  const increment = function() {
+    privateValue += 1;
+    console.log(privateValue);
+  };
+
+  return {
+    increment,
+  };
+};
+
+const counterA = createCounter();
+console.log(counterA)
+counterA.increment(); // 1
+
+counterA.increment(); // 2
+
+const counterB = createCounter();
+counterB.increment(); // 1
+counterB.increment(); // 2
+counterB.increment(); // 3
