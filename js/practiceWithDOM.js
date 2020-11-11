fe0fa43d22f5bfaf7731b5f0055aa52e9418e857
Yehor <65769717+Yehor-Rudzinskyi@users.noreply.github.com>
2020-11-11 18:53:59 +0200

    add HTML for DOM practice

--- js/practiceWithDOM.js
+++ js/practiceWithDOM.js
@@ -1,4 +1,13 @@
 const tittleRef = document.querySelector('.page-tittle');
 console.log(tittleRef);
-// Ищем через любой вид селектора. Можно, через селектор тега - h2, а можно
+// Ищем через любой вид селектора(до первого совпадения!).
+// И получаем обычную ссылку на объект!
+// Можно, через селектор тега - h2, а можно
 // и через селектор класса - .page-tittle Вообщем обращаемся через селектор
+
+// Ищем все относящиеся
+const allLinks = document.querySelectorAll('.nav-item-link');
+console.log(allLinks);
+
+// Свойчтво textContent - задаёт текст
+tittleRef.textContent = 'JS - easy!';
