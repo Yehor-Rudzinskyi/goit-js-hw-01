cc60000135e5249a47b0ed2ff13bc33ceb244c92
Yehor <65769717+Yehor-Rudzinskyi@users.noreply.github.com>
2020-11-11 18:14:58 +0200

    add DOM- practice

---
+++ js/practiceWithDOM.js
@@ -0,0 +1,4 @@
+const tittleRef = document.querySelector('.page-tittle');
+console.log(tittleRef);
+// Ищем через любой вид селектора. Можно, через селектор тега - h2, а можно
+// и через селектор класса - .page-tittle Вообщем обращаемся через селектор
